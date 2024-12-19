e65e7ed3273ea7c2193d90ea05ded0b50693b553
Raman Dhaliwal <dramandeepkaur202@gmail.com>
2024-12-18 21:44:44 -0600

    add some test and Apply some Style in Feature1 Branch

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Assignment 4: Building a React Native Project on Android Emulator with Source Control</Text>
+      <Text style={styles.name}>Raman Dhaliwal</Text>
+      <Text style={styles.bio}>Post-Graduate Student in Mobile App Development</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +19,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  name: {
+    fontSize: 24,
+    fontWeight: 'bold',
+},
+bio: {
+    fontSize: 16,
+    color: '#666',
+    textAlign: 'center',
+    marginHorizontal: 20,
+},
 });
