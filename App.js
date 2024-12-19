53181dacd0f8c19def640ea1826cdbe1fa90bb9f
Raman Dhaliwal <dramandeepkaur202@gmail.com>
2024-12-18 21:38:08 -0600

    Initial Commit- Branch Master

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Assignment 4: Building a React Native Project on Android Emulator with Source Control</Text>
       <StatusBar style="auto" />
     </View>
   );
